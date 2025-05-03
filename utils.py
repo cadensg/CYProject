b2552586ddab3f7e032699cfac3bfa9d3e065187
cadensg <cadengreco0@gmail.com>
2025-05-03 16:18:29 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 def search_track(sp, query):
-    result = sp.search(q=query, type='track', limit=1)
-    tracks = result.get('tracks', {}).get('items', [])
-    if tracks:
-        return {
-            'uri': tracks[0]['uri'],
-            'name': tracks[0]['name'],
-            'artist': tracks[0]['artists'][0]['name'],
-            'popularity': tracks[0]['popularity']
-        }
-    return None
+    try:
+        result = sp.search(q=query, type='track', limit=1)
+        tracks = result.get('tracks', {}).get('items', [])
+        if tracks:
+            return {
+                'uri': tracks[0]['uri'],
+                'name': tracks[0]['name'],
+                'artist': tracks[0]['artists'][0]['name'],
+                'popularity': tracks[0]['popularity']
+            }
+        return None
+    except Exception as e:
+        print(f"Error searching for track '{query}': {e}")
+        return None
