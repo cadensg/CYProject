ff3be647dacef8c30102ed6dce943539a907a831
cadensg <cadengreco0@gmail.com>
2025-05-03 16:45:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def callback():
     except Exception as e:
         print("General error:", e)
         return "An error occurred", 500
-"""
+
 
     html_output = ""
     for cluster, songs in recommendations.items():
@@ -51,6 +51,6 @@ def callback():
         html_output += "</ul>"
 
     return html_output
-
+"""
 if __name__ == "__main__":
     app.run(debug=True)
